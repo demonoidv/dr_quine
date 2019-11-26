9e7c0d3c9131f2ce6b6afc0e46c3f9daf3452645
Valentin Sporer <v@sporer.fr>
2019-11-26 11:02:00 +0100

    Add fclose() call in Grace

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define CODE "#include <stdio.h>%1$c%1$c#define CODE %3$c%4$s%3$c%1$c/*%1$c	This is a comment.%1$c*/%1$c#define CHILD %3$cGrace_kid.c%3$c%1$c%1$c#define MAIN(x) int	main(void)%2$c%1$c{%2$c%1$c	FILE	*handle = fopen(CHILD, %3$cw%3$c);%2$c%1$c	if (handle)%2$c%1$c	{%2$c%1$c		fprintf(handle, CODE, '%2$cn', '%2$c%2$c', '%3$c', CODE);%2$c%1$c		return 0;%2$c%1$c	}%2$c%1$c	return -1;%2$c%1$c}%1$c%1$cMAIN(x)%1$c"
+#define CODE "#include <stdio.h>%1$c%1$c#define CODE %3$c%4$s%3$c%1$c/*%1$c	This is a comment.%1$c*/%1$c#define CHILD %3$cGrace_kid.c%3$c%1$c%1$c#define MAIN(x) int	main(void)%2$c%1$c{%2$c%1$c	FILE	*handle = fopen(CHILD, %3$cw%3$c);%2$c%1$c	if (handle)%2$c%1$c	{%2$c%1$c		fprintf(handle, CODE, '%2$cn', '%2$c%2$c', '%3$c', CODE);%2$c%1$c		fclose(handle);%2$c%1$c		return 0;%2$c%1$c	}%2$c%1$c	return -1;%2$c%1$c}%1$c%1$cMAIN(x)%1$c"
 /*
 	This is a comment.
 */
@@ -12,6 +12,7 @@
 	if (handle)\
 	{\
 		fprintf(handle, CODE, '\n', '\\', '"', CODE);\
+		fclose(handle);\
 		return 0;\
 	}\
 	return -1;\
