96a489281057d89b06740cc90353629350ac6cb9
Valentin SPORER <vsporer@e1r4p17.42.fr>
2019-12-03 18:12:33 +0100

    [ASM] Add some improvement to Grace - WIP

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,25 +1,43 @@
 global _main
-%define MAIN(x)\
-_main:\
-	mov		rdi, code\
-	mov		rsi, 0x0a\
-	mov		rdx, 0x22\
-	mov		rcx, code\
-	call	_printf\
-	xor		eax, eax\
-	ret\
+%macro MAIN 0
+_main:
+	push		rax
+	mov			rdi, kids_name
+	mov			rsi, access
+	call		_fopen
+	cmp			eax, 0
+	je			.end
+	mov			rdi, rax
+	push		rdi
+	mov			rax, 0
+	mov			rsi, code
+	mov			rdx, 0x0a
+	mov			rcx, 0x22
+	mov			rbx, code
+	; push		rbx
+	call		_fprintf
+	pop			rdi
+	call		_fclose
+	; xor			eax, eax
+.end:
+	pop			rbx
+	ret
+%endmacro
 
-%define KID: db		"Grace_kid.s", 0
-%define CODE: db	"", 0
+%define KID		kids_name: db "Grace_kid.s", 0
+%define CODE	code: db "#1: %c, #2: %c, #3: %s", 0
 
 section .data
 KID
 CODE
+access:	db		"w", 0
 
 section .text
 ;
 ; This is a comment.
 ;
-extern _printf
+extern _fprintf
+extern _fopen
+extern _fclose
 
-MAIN()
+MAIN
