03ca1717c5cad29f7289dd4323719e792b340850
Valentin Sporer <v@sporer.fr>
2019-12-09 16:29:26 +0100

    [ASM] Fix linker warning in Colleen.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,7 +1,7 @@
 global _main
 
 section .data
-code: db	"global _main%1$c%1$csection .data%1$ccode: db	%2$c%3$s%2$c, 0%1$c%1$csection .text%1$c;%1$c; This is a comment.%1$c;%1$cextern _printf%1$c_print_code:%1$c	mov		rdi, code%1$c	mov		rsi, 0x0a%1$c	mov		rdx, 0x22%1$c	mov		rcx, code%1$c	call	_printf%1$c	ret%1$c%1$c_main:%1$c	;%1$c	; This is another comment.%1$c	;%1$c	call	_print_code%1$c	xor		eax, eax%1$c	ret%1$c", 0
+code: db	"global _main%1$c%1$csection .data%1$ccode: db	%2$c%3$s%2$c, 0%1$c%1$csection .text%1$c;%1$c; This is a comment.%1$c;%1$cextern _printf%1$c_print_code:%1$c	lea		rdi, [rel code]%1$c	mov		rsi, 0x0a%1$c	mov		rdx, 0x22%1$c	lea		rcx, [rel code]%1$c	call	_printf%1$c	ret%1$c%1$c_main:%1$c	;%1$c	; This is another comment.%1$c	;%1$c	call	_print_code%1$c	xor		eax, eax%1$c	ret%1$c", 0
 
 section .text
 ;
@@ -9,10 +9,10 @@ section .text
 ;
 extern _printf
 _print_code:
-	mov		rdi, code
+	lea		rdi, [rel code]
 	mov		rsi, 0x0a
 	mov		rdx, 0x22
-	mov		rcx, code
+	lea		rcx, [rel code]
 	call	_printf
 	ret
 
